d5fd418b12f7d1fe4d4082969c246a3fea71727e
Ema <aemalorenzo@gmail.com>
2019-09-01 15:02:10 -0300

    ESLint + Prettier integration

--- App.js
+++ App.js
@@ -6,40 +6,16 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import { Platform, StyleSheet, Text } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
-
-export default class App extends Component {
-  render() {
-    return (
-      <LinearGradient start={{x: 0, y: 1}} end={{x: 1, y: 0}} colors={['#E53E32', '#C6178E']} style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Button
-          icon={
-            <Icon
-              name="arrow-right"
-              size={15}
-              color="white"
-            />
-          }
-          title="Button with icon component"
-        />
-      </LinearGradient>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -59,3 +35,24 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+const App = () => {
+  return (
+    <LinearGradient
+      start={{ x: 0, y: 1 }}
+      end={{ x: 1, y: 0 }}
+      colors={['#E53E32', '#C6178E']}
+      style={styles.container}
+    >
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <Text style={styles.instructions}>{instructions}</Text>
+      <Button
+        icon={<Icon name="arrow-right" size={15} color="white" />}
+        title="Button with icon component"
+      />
+    </LinearGradient>
+  );
+};
+
+export default App;
